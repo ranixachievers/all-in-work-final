cd09678379a01740a41ecce939d990670fd6cc76
TIGER <141488144+tiger7815@users.noreply.github.com>
2023-11-14 14:31:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = "6518790189:AAH18Rn2TSpRV2cDrFVF-Ruqy9CrChZ-F1s"
-    API_ID = "20717621"
-    API_HASH = "a68af155e7f0c79b1baab1f185f4181d"
-    AUTH_USERS = '5934830127,1085174050'
+    BOT_TOKEN = "6731306687:AAH7FhCo9vBpM_PX2blym_RgbEKnWoCAcNA"
+    API_ID = "22609670"
+    API_HASH = "3506d8474ad1f4f5e79b7c52a5c3e88d"
+    AUTH_USERS = '5934830127,6981453498'
 
