a919d79c17b34ff6184cabb690e8a0ff129c6e02
ranixachievers <raniachievers@outlook.com>
2024-06-26 10:22:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,7 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = "6731306687:AAH7FhCo9vBpM_PX2blym_RgbEKnWoCAcNA"
+    BOT_TOKEN = "7275956983:AAEED1fQDiv1ngz-W73bcIeuqebYGwU6Yos"
     API_ID = "22609670"
     API_HASH = "3506d8474ad1f4f5e79b7c52a5c3e88d"
-    AUTH_USERS = '5934830127,6981453498'
-
+    AUTH_USERS = '7078486618,6981453498'
